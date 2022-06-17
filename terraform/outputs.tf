43b795694e390e9be533610d4007f8161f1ef9c5
Harsha Jain <harshajain@Harshas-MacBook-Pro.local>
2022-06-17 10:04:41 +0530

    output variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "ami_id" {
     description = "This is the K8S ami ID ....."
     value = data.aws_ami.my_ubuntu.id
 }
+
+output "owner" {
+    description = "This is the K8S ami OWNER ....."
+    value = data.aws_ami.my_ubuntu.owner
+}
