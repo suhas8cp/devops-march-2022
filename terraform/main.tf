edeb98511bb217fb3335545b45c1288576f143af
Harsha Jain <harshajain@Harshas-MacBook-Pro.local>
2022-06-17 09:08:42 +0530

    input variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,7 @@
-locals {
-    my_ami = "ami-068257025f72f470d"
-    my_instance_type = "t2.micro"
-}
-
 resource "aws_instance" "my_instance1" {
-    ami = local.my_ami
-    instance_type = local.my_instance_type
+    ami = var.my_ami
+    instance_type = var.my_instance_type
     tags = {
         Name = "terraform-instance1"
-    }
-}
-
-resource "aws_instance" "my_instance2" {
-    ami = local.my_ami
-    instance_type = local.my_instance_type
-    tags = {
-        Name = "terraform-instance2"
     }
 }
---
+++ terraform/varibales.tf
@@ -0,0 +1,9 @@
+variable "my_ami" {
+    type = string
+    default = "ami-068257025f72f470d"
+}
+
+variable "my_instance_type" {
+    type = string
+    default = "t2.micro"
+}
